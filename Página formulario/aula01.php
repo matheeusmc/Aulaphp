981d3fddf8c1bfb36d1e547710ecef648e7b2a59
Wellington da Silva <matheuslimadasilva0@gmail.com>
2024-10-30 21:38:41 -0300

    php

---
+++ Página formulario/aula01.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+<h1>Testando codigo em php</h1>
+
+<?php 
+
+$nome = "Matheus lima";
+$idade = 26;
+$sexo = "Masculino";
+
+echo "Nome: $nome, Idade: $idade, Sexo: $sexo";
+
+?>
+
+
+</body>
+</html>
